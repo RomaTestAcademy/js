ff79ec3e4f5a19da3d5c10deafd91aae6a6b77b5
RomaBilka <roman_biliak@i.ua>
2018-05-13 12:04:45 +0300

    add files

---
+++ js/fight.js
@@ -0,0 +1,8 @@
+function fight(fighter, improvedFighter, ...point){		
+	while(true){
+		if(fighter.health > 0 && improvedFighter.health > 0) fighter.hit(improvedFighter, point);
+		else break;
+		if(fighter.health > 0 && improvedFighter.health > 0) improvedFighter.doubleHit(fighter, point);
+		else break;
+	}
+}
---
+++ js/fighter.js
@@ -0,0 +1,28 @@
+class Fighter{
+	constructor(name='noName', power=1, health=100) {
+		this.name = name;
+		this.power = power;
+		this.health = health;
+	}
+	setDamage(damage){
+		if(this.health > damage){
+			this.health -= damage;
+		}else{
+			this.health = 0;
+		}
+		console.log(`${this.name} health: ${this.health}`);
+	}
+	hit(enemy,point){
+		for(let i in point){
+			if (enemy.health > 0){
+				enemy.setDamage(point[i] * this.power);
+			}else{
+				this.gameOver(this.name, enemy.name);
+				break;
+			}			
+		}
+	}
+	gameOver(won, dead){
+		console.log(`GAME OVER \n${won.toUpperCase()} - won \n${dead.toUpperCase()} - dead`);
+	}
+}
